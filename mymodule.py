cf3c138164ba6395800966e3d1fd81e6c284b5f2
limcwei <62639046+limcwei@users.noreply.github.com>
2020-03-25 15:15:03 +0800

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -4,7 +4,7 @@ import json
 api_key = "xxxxxxx"
 
 def get_weather(city):
-  sock = urlopen("http://api.openweathermpa.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
+  sock = urlopen("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key)
   result = sock.read()
   sock.close()
   weather = json.loads(result)
